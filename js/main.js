807d51bad87a2e3cfad8f0af04bb932344fbbcac
unknown <andrew99154@gmail.com>
2020-09-01 20:57:12 +0800

    addyearActivity

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 function init() {
-    var menuBtn = document.getElementById("menuBtn")
-    var menuList = document.getElementById("navPanel")
-    var menuClose = document.getElementById("menuClose")
-    menuBtn.onclick = function () {
-      menuList.classList.toggle("visible")
-    }
-    menuClose.onclick = function () {
-      menuList.classList.toggle("visible")
-    }
-
+  var menuBtn = document.getElementById("menuBtn");
+  var menuList = document.getElementById("navPanel");
+  var menuClose = document.getElementById("menuClose");
+  menuBtn.onclick = function () {
+    menuList.classList.toggle("visible");
+  };
+  menuClose.onclick = function () {
+    menuList.classList.toggle("visible");
+  };
+}
+function DayNightBgChange() {
+  var today = new Date();
+  now = today.getHours();
+  var ele = document.getElementById("nice");
+  if ((now >= 18 && now <= 24) || (now >= 0 && now <= 6)) {
+  } else {
   }
+}
